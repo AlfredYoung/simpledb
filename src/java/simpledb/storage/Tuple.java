59e8873ca964ebf59b60c9e05df3e303c3d23f74
Alfred Yang <1150714183@qq.com>
2022-05-08 10:37:16 +0800

    LAB1

---
+++ src/java/simpledb/storage/Tuple.java
@@ -0,0 +1,148 @@
+package simpledb.storage;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /* 元组的描述信息 */
+    private TupleDesc td;
+    /* 元组id */
+    private RecordId rid;
+    /* 表示一条记录的所有字段 */
+    private Field[] fields;
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td
+     *            the schema of this tuple. It must be a valid TupleDesc
+     *            instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        // some code goes here
+        /* 构造方法 */
+        this.td = td;
+        fields = new Field[td.numFields()];
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+        return td;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        // some code goes here
+        return rid;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid
+     *            the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        // some code goes here
+        this.rid = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i
+     *            index of the field to change. It must be a valid index.
+     * @param f
+     *            new value for the field.
+     */
+    public void setField(int i, Field f) {
+        // some code goes here
+        fields[i] = f;
+    }
+
+    /**
+     * @return the value of the ith field, or null if it has not been set.
+     *
+     * @param i
+     *            field index to return. Must be a valid index.
+     */
+    public Field getField(int i) {
+        // some code goes here
+        return fields[i];
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     *
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     *
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        // some code goes here
+        /* 重写toString方法 */
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(fields[0].toString());
+        for  (int i=1; i<fields.length; i++) {
+            sb.append("\t").append(fields[i].toString());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * @return
+     *        An iterator which iterates over all the fields of this tuple
+     * */
+    public Iterator<Field> fields()
+    {
+        // some code goes here
+        /*迭代器类，用于遍历表*/
+        return new Iterator<Field>() {
+            private int idx = -1;
+
+            @Override
+            public boolean hasNext() {
+                return idx+1 < fields.length;
+            }
+            @Override
+            public Field next() {
+                if (++idx == fields.length) {
+                    throw new NoSuchElementException();
+                } else {
+                    return fields[idx];
+                }
+
+            }
+            @Override
+            public void remove() {
+
+            }
+        };
+    }
+
+    /**
+     * reset the TupleDesc of this tuple (only affecting the TupleDesc)
+     * */
+    public void resetTupleDesc(TupleDesc td)
+    {
+        // some code goes here
+        this.td = td;
+    }
+}
